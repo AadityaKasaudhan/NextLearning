cbecb101b6a091c35acddcfe1e9100916360ffc5
AadityaKasaudhan <adityakas1907@gmail.com>
2024-09-06 02:25:50 +0530

    Nextjs Website

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+// pages/index.js
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <h1>Welcome to the Homepage!</h1>
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
